da8dce2649923ba7bf79e9eff9f2427a4eedf8c0
Alphaxard Gacuuru <alphaxardgacuuru47@gmail.com>
2026-02-08 00:48:54 +0300

    fix: property service charge

--- app/Http/Resources/PropertyResource.php
+++ app/Http/Resources/PropertyResource.php
@@ -41,7 +41,7 @@ class PropertyResource extends JsonResource
 			"name" => $this->name,
 			"location" => $this->location,
 			"depositFormula" => $this->deposit_formula,
-			"serviceCharge" => $this->service_charge?->service,
+			"serviceCharge" => $this->service_charge,
 			"waterBillRateCouncil" => $this->water_bill_rate->council,
 			"waterBillRateBorehole" => $this->water_bill_rate->borehole,
 			"waterBillRateTanker" => $this->water_bill_rate->tanker,
